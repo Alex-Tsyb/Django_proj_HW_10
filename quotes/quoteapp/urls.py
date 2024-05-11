3de1053d23f1a178845da69a3f0a427404d03b86
Alex-Tsyb <113551674+Alex-Tsyb@users.noreply.github.com>
2024-05-11 15:13:04 +0300

    env variables, reset user password

--- quotes/quoteapp/urls.py
+++ quotes/quoteapp/urls.py
@@ -4,12 +4,13 @@ from . import views
 app_name = 'quoteapp'
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('tag/', views.tag, name='tag'),
     path('author/', views.author, name='author'),
     path('quote/', views.quote, name='quote'),
     path('detail/<int:quote_id>', views.detail, name='detail'),
     path('author_detail/<int:author_id>', views.author_detail, name='author_detail'),
     path('delete/<int:quote_id>', views.delete_quote, name='delete'),
-    path('', views.main, name='main'),
-    re_path('', views.main, name='main'),
+    #path('', views.main, name='main'),
+    #re_path('', views.main, name='main'),
 ]
